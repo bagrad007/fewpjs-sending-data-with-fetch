45cc81f7b6f21bad73dc8f72e7d9878b3aa9b4da
Luke Gill <bagrad007@gmail.com>
2021-04-19 12:58:54 -0400

    commit tree 55e8c5410794f8e4e8cdbfe246e53ea26ce489de

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Add your code here
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+// };
+
+// let configObj = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+// };
+
+// fetch("http://localhost:3000/dogs", configObj)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     });
+
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json", "Accept": "application/json" },
+        body: JSON.stringify({ name: name, email: email })
+    }).then(res => res.json())
+        .then(obj => {
+            console.log(obj);
+            const body = document.querySelector("body");
+            body.textContent = obj.id;
+        })
+        .catch(err => {
+            console.log(err.message)
+            const body = document.querySelector("body")
+            body.textContent = err.message
+        })
+}
+
+
+submitData("John", "john@email.com");
